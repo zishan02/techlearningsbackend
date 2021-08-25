91db273abcc31d7b9e9f5af0c3b782a16a0e2cd5
Zishan <zishanmohammed02@gmail.com>
2021-08-25 23:33:31 +0530

    first commit

---
+++ src/main/java/com/techlearnings/techlearnings/TechlearningsApplication.java
@@ -0,0 +1,13 @@
+package com.techlearnings.techlearnings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechlearningsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechlearningsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techlearnings/techlearnings/TechlearningsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techlearnings.techlearnings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechlearningsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
